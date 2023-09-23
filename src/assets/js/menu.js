cf9e8729e4b6f0893cf9587e657f0799d8946d01
julia <noocgs@gmail.com>
2023-09-23 16:53:48 +0900

    헤더완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,19 @@
 export function menu() {
-    console.log(menu)
+
+    const body = document.querySelector('body');
+    const headerToggle = document.querySelector(".header__nav__mo");
+    const headerNav = document.querySelector(".header__nav");
+
+    headerToggle.addEventListener("click", () => {
+    
+    !headerToggle.classList.contains('active')
+        ? headerToggle.classList.add('active') 
+        & body.classList.add('scroll__hidden')
+        & headerNav.classList.add('active')
+        : headerToggle.classList.remove('active') 
+        & body.classList.remove('scroll__hidden')
+        & headerNav.classList.remove('active')
+    });
+
 }
+

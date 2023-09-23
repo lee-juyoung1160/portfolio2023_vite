06748229ae52787cd4f224c1418cc3df3caa8325
julia <noocgs@gmail.com>
2023-09-23 15:34:33 +0900

    레이아웃 완료:

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,11 @@
-console.log("시작하기")
+import { menu } from "./menu.js";
+import { link } from "./link.js";
+import { smooth } from "./smooth.js";
+import { cursor } from "./cursor.js";
+
+window.addEventListener("load", function () {
+    cursor();
+    smooth();
+    link();
+    menu();
+});

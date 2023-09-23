1a023ad36e2d8724a159652ce41767dd3ccd53a5
julia <noocgs@gmail.com>
2023-09-23 17:06:24 +0900

    기타 효과 추가 및 skip 히든처리

--- src/assets/js/cursor.js
+++ src/assets/js/cursor.js
@@ -1,87 +1,87 @@
 export function cursor() {
-    console.log(cursor);
-    // const cursor = document.querySelector('#cursor');
-    // const cursorCircle = cursor.querySelector('.cursor__circle');
 
-    // const mouse = { x: -100, y: -100 }; // mouse pointer's coordinates
-    // const pos = { x: 0, y: 0 }; // cursor's coordinates
-    // const speed = 0.2; // between 0 and 1
+    const cursor = document.querySelector('#cursor');
+    const cursorCircle = cursor.querySelector('.cursor__circle');
 
-    // const updateCoordinates = e => {
-    //     mouse.x = e.clientX + 5;
-    //     mouse.y = e.clientY + 5;
-    // }
+    const mouse = { x: -100, y: -100 }; // mouse pointer's coordinates
+    const pos = { x: 0, y: 0 }; // cursor's coordinates
+    const speed = 0.2; // between 0 and 1
 
-    // window.addEventListener('mousemove', updateCoordinates);
+    const updateCoordinates = e => {
+        mouse.x = e.clientX + 5;
+        mouse.y = e.clientY + 5;
+    }
 
-    // function getAngle(diffX, diffY) {
-    //     return Math.atan2(diffY, diffX) * 180 / Math.PI;
-    // }
+    window.addEventListener('mousemove', updateCoordinates);
 
-    // function getSqueeze(diffX, diffY) {
-    //     const distance = Math.sqrt(
-    //         Math.pow(diffX, 2) + Math.pow(diffY, 2)
-    //     );
-    //     const maxSqueeze = 0.15;
-    //     const accelerator = 1500;
-    //     return Math.min(distance / accelerator, maxSqueeze);
-    // }
+    function getAngle(diffX, diffY) {
+        return Math.atan2(diffY, diffX) * 180 / Math.PI;
+    }
 
-    // const updateCursor = () => {
-    // const diffX = Math.round(mouse.x - pos.x);
-    // const diffY = Math.round(mouse.y - pos.y);
+    function getSqueeze(diffX, diffY) {
+        const distance = Math.sqrt(
+            Math.pow(diffX, 2) + Math.pow(diffY, 2)
+        );
+        const maxSqueeze = 0.15;
+        const accelerator = 1500;
+        return Math.min(distance / accelerator, maxSqueeze);
+    }
 
-    // pos.x += diffX * speed;
-    // pos.y += diffY * speed;
+    const updateCursor = () => {
+    const diffX = Math.round(mouse.x - pos.x);
+    const diffY = Math.round(mouse.y - pos.y);
 
-    // const angle = getAngle(diffX, diffY);
-    // const squeeze = getSqueeze(diffX, diffY);
+    pos.x += diffX * speed;
+    pos.y += diffY * speed;
 
-    // const scale = 'scale(' + (1 + squeeze) + ', ' + (1 - squeeze) +')';
-    // const rotate = 'rotate(' + angle +'deg)';
-    // const translate = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
+    const angle = getAngle(diffX, diffY);
+    const squeeze = getSqueeze(diffX, diffY);
 
-    // cursor.style.transform = translate;
-    //     cursorCircle.style.transform = rotate + scale;
-    // };
+    const scale = 'scale(' + (1 + squeeze) + ', ' + (1 - squeeze) +')';
+    const rotate = 'rotate(' + angle +'deg)';
+    const translate = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
 
-    // function loop() {
-    //     updateCursor();
-    //     requestAnimationFrame(loop);
-    // }
+    cursor.style.transform = translate;
+        cursorCircle.style.transform = rotate + scale;
+    };
 
-    // requestAnimationFrame(loop);
+    function loop() {
+        updateCursor();
+        requestAnimationFrame(loop);
+    }
 
-    // /* a 태그 전체에 cursor-class="arrow" 추가 */
-    // var aTags = Array.from(document.getElementsByTagName("a"));
-    // aTags.forEach(function(aTag) {
-    //     aTag.setAttribute("cursor-class", "arrow");
-    // });
+    requestAnimationFrame(loop);
 
-    // var aTags = Array.from(document.querySelectorAll(".mySkill li"));
-    // aTags.forEach(function(aTag) {
-    //     aTag.setAttribute("cursor-class", "arrow");
-    // });
+    /* a 태그 전체에 cursor-class="arrow" 추가 */
+    var aTags = Array.from(document.getElementsByTagName("a"));
+    aTags.forEach(function(aTag) {
+        aTag.setAttribute("cursor-class", "arrow");
+    });
 
-    // /* 게시글 수정버튼 cursor-class="arrow" 삭제 */
-    // var modifys = Array.from(document.getElementsByClassName("modify"));
-    // modifys.forEach(function(modify) {
-    //     modify.removeAttribute("cursor-class", "arrow");
-    // });
+    var aTags = Array.from(document.querySelectorAll(".mySkill li"));
+    aTags.forEach(function(aTag) {
+        aTag.setAttribute("cursor-class", "arrow");
+    });
 
-    // const cursorModifiers = document.querySelectorAll('[cursor-class]');
+    /* 게시글 수정버튼 cursor-class="arrow" 삭제 */
+    var modifys = Array.from(document.getElementsByClassName("modify"));
+    modifys.forEach(function(modify) {
+        modify.removeAttribute("cursor-class", "arrow");
+    });
 
-    // cursorModifiers.forEach(curosrModifier => {
-    //     curosrModifier.addEventListener('mouseenter', function() {
-    //         const className = this.getAttribute('cursor-class');
-    //         cursor.classList.add(className);
-    //     });
+    const cursorModifiers = document.querySelectorAll('[cursor-class]');
 
-    //     curosrModifier.addEventListener('mouseleave', function() {
-    //         const className = this.getAttribute('cursor-class');
-    //         cursor.classList.remove(className);
-    //     });
-    // });
+    cursorModifiers.forEach(curosrModifier => {
+        curosrModifier.addEventListener('mouseenter', function() {
+            const className = this.getAttribute('cursor-class');
+            cursor.classList.add(className);
+        });
+
+        curosrModifier.addEventListener('mouseleave', function() {
+            const className = this.getAttribute('cursor-class');
+            cursor.classList.remove(className);
+        });
+    });
 }
 
 
